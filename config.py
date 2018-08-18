60abd93321be578c851449a0462b4c3d9921dd90
Suren Murugavel <suren.murugavel@gmail.com>
2018-08-18 20:37:05 +0530

    Added MYSQL config

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'can never guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://pi_user:raspberry@localhost/FB_DATABASE'
