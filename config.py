4c0b39085c702d8044fc3c45fd04ed584b265c3b
Suren Murugavel <suren.murugavel@gmail.com>
2018-08-30 11:55:13 +0530

    Added Migration directories: flask db init

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'can never guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://pi_user:raspberry@localhost/FB_DATABASE'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
