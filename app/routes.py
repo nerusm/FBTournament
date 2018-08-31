73d880b03ad71811840451b364feba5f4123e95c
Suren Murugavel <suren.murugavel@gmail.com>
2018-08-31 13:04:36 +0530

    Adding login functions.

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,12 @@ __author__ = 'suren'
 from app import app
 from flask import render_template, request, flash,redirect, url_for
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
+
+# # Start from Requiring Users To Login int he Tutorial:
+    # https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-v-user-logins
 
 @app.route('/')
 @app.route('/index')
@@ -16,12 +21,22 @@ def index():
 
 @app.route('/login', methods= ['GET', 'POST'])
 def login():
-    form = LoginForm(request.form)
-    print(form.username.data)
-    print(form.validate_on_submit())
-    if form.validate_on_submit():
-        print('inside if')
-        flash('Login Request Received for User: {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
-    print('hi')
+    form = LoginForm(request.form)
+    # print(form.username.data)
+    # print(form.validate_on_submit())
+    if form.validate_on_submit():
+       user = User.query.filter_by(username=form.username.data).first()
+       if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+       else:
+           login_user(user,remember=form.remember_me.data)
+           return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user
+    return redirect(url_for('index'))
